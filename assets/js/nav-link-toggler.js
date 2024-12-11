b723dd419200e015ef9c4e4773e0d7cb3ac2a4ce
Azeez-Abiola <Abiolaquadri111@gmail.com>
2024-12-11 16:38:59 +0100

    pushed

--- assets/js/nav-link-toggler.js
+++ assets/js/nav-link-toggler.js
@@ -2,21 +2,21 @@
 
 $(document).ready(function(){
   
-    $(".main-nav-link").click(function(){
-      // self clicking close
-      if($(this).hasClass("active")){
-        $(this).removeClass("active");
-        $(this).next(".sub-nav-link").removeClass("active").slideUp()
-        $(this).find("i").removeClass("fa-minus").addClass("fa-plus")	
-      }
+  $(".main-nav-link").click(function(){
+    // self clicking close
+    if($(this).hasClass("active")){
+      $(this).removeClass("active");
+      $(this).next(".sub-nav-link").removeClass("active").slideUp()
+      $(this).find("i").removeClass("fa-minus").addClass("fa-plus")	
+    }
 
-      else{
-        $(".nav-link .main-nav-link").removeClass("active");
-        $(".nav-link .sub-nav-link").removeClass("active").slideUp()
-        $(".nav-link .main-nav-link i").removeClass("fa-minus").addClass("fa-plus");
-        $(this).addClass("active");
-        $(this).next(".sub-nav-link").addClass("active").slideDown()
-        $(this).find("i").removeClass("fa-plus").addClass("fa-minus")
-      }
-    })
+    else{
+      $(".nav-link .main-nav-link").removeClass("active");
+      $(".nav-link .sub-nav-link").removeClass("active").slideUp()
+      $(".nav-link .main-nav-link i").removeClass("fa-minus").addClass("fa-plus");
+      $(this).addClass("active");
+      $(this).next(".sub-nav-link").addClass("active").slideDown()
+      $(this).find("i").removeClass("fa-plus").addClass("fa-minus")
+    }
   })
+})
