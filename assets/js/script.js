380e1ce6b6945e7ed8dd25d8d9837eec78ddeda1
Azeez-Abiola <Abiolaquadri111@gmail.com>
2024-12-16 17:20:20 +0100

    updated routing and Ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,13 @@ $(document).ready(function(){
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  const faqItems = document.querySelectorAll('.faq-item');
+  
+  faqItems.forEach(item => {
+      const question = item.querySelector('.faq-question');
+      question.addEventListener('click', () => {
+          item.classList.toggle('active');
+      });
+  });
+});
