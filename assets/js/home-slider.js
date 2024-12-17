321a63000b0ad24fbd2d82435c44a71d47ae2ee9
Azeez-Abiola <Abiolaquadri111@gmail.com>
2024-12-17 14:08:05 +0100

    updated

--- assets/js/home-slider.js
+++ assets/js/home-slider.js
@@ -1,20 +1,20 @@
 /*--------------- Home Slider ---------------*/ 
-var swiper = new Swiper(".home-slider", {
-
-    loop:true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false, 
-    },
-
-    pagination: {
-      el: ".swiper-pagination1",
-      clickable:true,
-    },
-
+document.addEventListener('DOMContentLoaded', function() {
+  var swiper = new Swiper('.home-slider', {
+    loop: true,
+    grabCursor: true,
+    speed: 1000,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-
-}); 
+    pagination: {
+      el: '.swiper-pagination1',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+});
